348890fdf0f5a78179971ccbb56612719aa5295f
Elkin Waldo Alarca Santamaria <elkinalarca@gmail.com>
2019-10-09 20:26:41 -0500

    Learn React
    React JS basics

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import './App.css';
 
-function HelloIsaac(props) {
+/* function HelloIsaac(props) {
   console.log(props)
   return (
-
     <div id="hello">
       <h3>{props.subtitle}</h3>
       {props.mytext}{props.text}{props.Titt}
     </div>
   )
-}
+} */
 
 //Escribir la misma funcion, pero ya con arrow function asignada a una const
 
 // const App = () => <div>This the second greetting: <HelloIsaac /></div>
 
 //Escribir la misma funcion, pero con definicion de Clase
+class HelloIsaac extends React.Component {
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div> This the third new greetting for my son: <HelloIsaac /> </div >
-//     )
-//   }
-// }
+  state = {
+    show: true
+  }
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}{this.props.text}{this.props.Titt}<br />
+          <button onClick={() => this.setState({ show: false })}>Toggle Show</button>
+        </div >
+      )
+    } else {
+      return <h1>{this.props.other}<br />
+        <button onClick={() => this.setState({ show: true })}>Returner</button>
+      </h1>
+
+    }
+  }
+}
 
 function App() {
   return (
     <div>
       The Component Isaac:
-      <HelloIsaac mytext="Hello Elkin" subtitle="First position" />
-      <HelloIsaac text="Isaac is very Genius" subtitle="Second position" />
-      <HelloIsaac Titt="Expert" subtitle="Third position" />
+      <HelloIsaac mytext="Hello Elkin" subtitle="First position" other="Another case" />
+      <HelloIsaac text="Isaac is very Genius" subtitle="Second position" other="Another case" />
+      <HelloIsaac Titt="Expert" subtitle="Third position" other="Another case" />
     </div>
   );
 }
