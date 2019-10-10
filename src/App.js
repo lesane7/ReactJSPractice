ea25a9851ccb85b076919ccabd4305f7b52eed05
Elkin Waldo Alarca Santamaria <elkinalarca@gmail.com>
2019-10-10 17:15:19 -0500

    Learning React Basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 /* function HelloIsaac(props) {
   console.log(props)
   return (
--- src/Appe.js
+++ src/Appe.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import tasks from './sample/tasks.json';
 import Tasks from './components/Tasks';
-
+import TaskForm from './components/TaskForm'
 
 class Appe extends Component {
 
@@ -12,13 +12,10 @@ class Appe extends Component {
 
     render() {
         return <div>
+            <TaskForm />
             <Tasks tasking={this.state.tasks} />
         </div>
-
     }
-
 }
-
-
 
 export default Appe;
